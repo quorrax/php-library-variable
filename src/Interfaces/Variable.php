d269ed534ef7dfc915e4b407707153ec5b0dcd07
Ilya Bakhlin <ilya.bakhlin@quorrax.com>
2020-02-27 07:33:17 +0100

    Adding and implementing the Variable interface.

---
+++ src/Interfaces/Variable.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Copyright © 2020 Quorrax, S. L.
+ *
+ * This file is part of quorrax/variable.
+ *
+ * quorrax/variable is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * quorrax/variable is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with quorrax/variable. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace Quorrax\Interfaces;
+
+/**
+ * @package Quorrax\Interfaces
+ */
+interface Variable
+{
+}
